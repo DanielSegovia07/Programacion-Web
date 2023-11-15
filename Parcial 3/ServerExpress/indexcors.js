df2193e97aaee542b604474668fbe38390ecce9d
DanielSegovia07 <danielsegovia671@gmail.com>
2023-11-15 12:13:47 -0600

    indexcorsserverexpress

--- Parcial 3/ServerExpress/indexcors.js
+++ Parcial 3/ServerExpress/indexcors.js
@@ -1,30 +1,12 @@
 const express = require('express');
 const cors = require('cors')
-const mysql2 = require('mysql2')
 const app = express();
 
 app.use(cors());
 
-//create connection database
-const connection = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'070803',
-    database: 'prueba2'
-})
-
-app.get('/',(req,res)=>{//consulta en el diagonal el nombre de la tabla
-    //console.log(req.query.idUsuario);
-    connection.query(
-        'SELECT * FROM personas',
-        function(err, results, fields){
-            console.log(results);
-            console.log(fields);
-
-            console.log(results);//array de objetos
-            res.json(results);
-        }
-    )
+app.get('/',(req,res)=>{//consulta
+    console.log(req.query);
+    res.json({ mensaje:"Servidor Express respondiendo a get"});
 });
 
 app.post('/',(req,res)=>{//alta
